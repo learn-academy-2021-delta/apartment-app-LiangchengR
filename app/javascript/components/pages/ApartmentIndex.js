22c2a72a1d2d7e93e70761865abb59ed03f3f4fc
LiangchengR <liangchengr@gmail.com>
2021-11-09 10:18:08 -0800

    added apartment index test

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
-import { Card, CardBody, CardTitle, CardText, Button} from "reactstrap"
+import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap"
+import { faRoad, faMap, faCity } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class ApartmentIndex extends Component {
 
@@ -8,19 +10,34 @@ export default class ApartmentIndex extends Component {
         const { apartments } = this.props;
         
         return (
-          <div>
-            {apartments.map((apartment) => (
-              <Card key={apartment.id}>
-                <CardBody>
-                  <CardTitle tag="h5">{apartment.street}</CardTitle>
-                  <CardText className="text-muted">
-                    street: {apartment.street} city: {apartment.city}, state:
-                    {apartment.state}
-                  </CardText>
+          <div className="apartment-index-container">
+            {apartments.map((apartment) => {
+              return (
+                <Card className="card" key={apartment.id}>
+                  <CardBody>
+                    <CardTitle tag="h5">{apartment.street}</CardTitle>
+                    <CardText className="text-muted">
+                      <span>
+                        <FontAwesomeIcon icon={faRoad} className="fa-road" />
+                        {apartment.street}
+                      </span>
+                      <br />
+                      <span>
+                        <FontAwesomeIcon icon={faCity} className="fa-city" />
+                        {apartment.city}
+                        <br />
+                      </span>
+                      <span>
+                        <FontAwesomeIcon icon={faMap} className="fa-map-pin" />
+                        {apartment.state}
+                        <br />
+                      </span>
+                    </CardText>
                     <Button>More Info</Button>
-                </CardBody>
-              </Card>
-            ))}
+                  </CardBody>
+                </Card>
+              )
+            })}
           </div>
         );
     }
